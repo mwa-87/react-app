2ddde7401e307ad80d3a643fe205054a96c0ff74
Marcin Walkowiak <m-w-a@o2.pl>
2020-11-13 20:12:58 +0100

    Updates

---
+++ precache-manifest.eb06575062937f18e33032b71635a18b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec70089d8b404188ab6a",
+    "url": "/react-app/static/css/main.6d94e022.chunk.css"
+  },
+  {
+    "revision": "ec70089d8b404188ab6a",
+    "url": "/react-app/static/js/main.ec70089d.chunk.js"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/react-app/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "32d2bc9684a3f939cf0fde033f61c4c5",
+    "url": "/react-app/index.html"
+  }
+];
